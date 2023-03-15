e6bb8f91fe0236d7a394ef58f8aad1ce880bb7c0
Alexander Mikhalev <alex@metacortex.engineer>
2023-03-15 19:57:51 +0000

    server start script for FC - fixed path

--- server/scripts/atomic-server-fs.sh
+++ server/scripts/atomic-server-fs.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-source /usr/local/cmdline.sh
+source /usr/bin/cmdline.sh
 
 ATOMIC_DOMAIN=get_cmd hostname
 /usr/bin/atomic-server --domain ${ATOMIC_DOMAIN} --data-dir /atomic/db --config-dir /atomic/config --log-level debug
