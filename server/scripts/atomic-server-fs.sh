c7663954303fcdba51077935def670b4ae394d78
Alexander Mikhalev <alex@metacortex.engineer>
2023-03-17 22:38:45 +0000

    minor script changes: https

--- server/scripts/atomic-server-fs.sh
+++ server/scripts/atomic-server-fs.sh
@@ -4,4 +4,4 @@ source /usr/bin/cmdline.sh
 
 ATOMIC_SERVER=$(get_cmd atomic_server)
 ATOMIC_PORT=$(get_cmd atomic_port)
-/usr/bin/atomic-server --port ${ATOMIC_PORT} --server-url ${ATOMIC_SERVER} --log-level debug
+/usr/bin/atomic-server --port ${ATOMIC_PORT} --server-url https://${ATOMIC_SERVER} --log-level debug
