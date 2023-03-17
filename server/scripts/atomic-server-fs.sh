cd8bed3ba4e0495b7998b554a7b915f3195d948f
Alexander Mikhalev <alex@metacortex.engineer>
2023-03-17 14:22:39 +0000

    minor script changes: add port to autostart

--- server/scripts/atomic-server-fs.sh
+++ server/scripts/atomic-server-fs.sh
@@ -3,4 +3,4 @@
 source /usr/bin/cmdline.sh
 
 ATOMIC_DOMAIN=$(get_cmd hostname)
-/usr/bin/atomic-server --domain ${ATOMIC_DOMAIN} --data-dir /atomic/db --config-dir /atomic/config --log-level debug
+/usr/bin/atomic-server --port 8080 --domain ${ATOMIC_DOMAIN} --data-dir /atomic/db --config-dir /atomic/config --log-level debug
