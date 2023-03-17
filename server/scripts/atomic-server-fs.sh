177d4a4fcc41bcd19782aed2f8ee9b585cb5d89a
Alexander Mikhalev <alex@metacortex.engineer>
2023-03-17 11:42:35 +0000

    minor script changes

--- server/scripts/atomic-server-fs.sh
+++ server/scripts/atomic-server-fs.sh
@@ -2,5 +2,5 @@
 
 source /usr/bin/cmdline.sh
 
-ATOMIC_DOMAIN=get_cmd hostname
+ATOMIC_DOMAIN=$(get_cmd hostname)
 /usr/bin/atomic-server --domain ${ATOMIC_DOMAIN} --data-dir /atomic/db --config-dir /atomic/config --log-level debug
